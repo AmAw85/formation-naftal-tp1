7bd41fca91b021ea06be152cd219f2bb385ed77b
AmAw85 <Ammi.Abdelouahab.85@gmail.com>
2023-06-05 14:11:25 +0100

    commit 2

---
+++ tp2.py
@@ -0,0 +1,19 @@
+a = int(input("donner A :"))
+b = int(input("donner B :"))
+
+print(a,"+",b,"=",a+b);
+print(a,"*",b,"=",a*b);
+print(a,"/",b,"=",a/b);
+print(a,"//",b,"=",a//b);
+print(a,"-",b,"=",a-b);
+
+is_black = False
+result = 120.9
+name = "amine"
+Constt = 6
+
+print(type(is_black))
+print(type(result))
+print(type(name))
+print(type(Constt))
+
